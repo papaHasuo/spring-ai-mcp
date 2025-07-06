2c08b8b0d3d04fa37b0da7a95eb3b09253bff675
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-06 22:20:47 +0900

    init commit

---
+++ src/main/java/jp/ne/papapa/spring_ai_mcp/SpringAiMcpApplication.java
@@ -0,0 +1,13 @@
+package jp.ne.papapa.spring_ai_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ne/papapa/spring_ai_mcp/SpringAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ne.papapa.spring_ai_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
