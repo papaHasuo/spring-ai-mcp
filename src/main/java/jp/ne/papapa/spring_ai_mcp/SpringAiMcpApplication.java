04006559eb7aeab085f1509b5aa1be3b6daf75d1
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-06 22:25:45 +0900

    メインクラスにMCPサーバのBeanを追加

--- src/main/java/jp/ne/papapa/spring_ai_mcp/SpringAiMcpApplication.java
+++ src/main/java/jp/ne/papapa/spring_ai_mcp/SpringAiMcpApplication.java
@@ -1,13 +1,21 @@
 package jp.ne.papapa.spring_ai_mcp;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiMcpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiMcpApplication.class, args);
+	}
+
+	@Bean
+	ToolCallbackProvider weatherTools(CalculateService calculateService) {
+		return  MethodToolCallbackProvider.builder().toolObjects(calculateService).build();
 	}
 
 }
